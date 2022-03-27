f3e17b0f76e00f4dd8eaa30c0258fb9a608d9d87
OscarSah <oscar@cybertekschool.com>
2022-03-27 16:51:09 +0300

    First Commit with Spartan API Get request

---
+++ src/test/java/com/cydeo/step_definitions/SpartanFlowSteps.java
@@ -0,0 +1,10 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.When;
+
+public class SpartanFlowSteps {
+    @When("User sends a request to Spartan API with id {int}")
+    public void user_sends_a_request_to_spartan_api_with_id(int id) {
+
+    }
+}
---
+++ src/test/resources/features/spartanFlow.feature
@@ -0,0 +1,4 @@
+Feature: Test Spartan API with complete CRUD operations
+  @wip
+  Scenario: Read data from Spartan API
+    When User sends a request to Spartan API with id 7
