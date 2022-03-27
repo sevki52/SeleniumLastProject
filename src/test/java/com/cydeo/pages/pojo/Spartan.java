a76edb2ede48da907b5fb696fb0dc5f191b02d14
OscarSah <oscar@cybertekschool.com>
2022-03-27 18:36:45 +0300

    POST request started sent

---
+++ src/test/java/com/cydeo/pages/pojo/Spartan.java
@@ -0,0 +1,106 @@
+
+package com.cydeo.pages.pojo;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class Spartan {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("gender")
+    @Expose
+    private String gender;
+    @SerializedName("phone")
+    @Expose
+    private Long phone;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Spartan() {
+    }
+
+    /**
+     * 
+     * @param gender
+     * @param phone
+     * @param name
+     * @param id
+     */
+    public Spartan(Integer id, String name, String gender, Long phone) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.phone = phone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Spartan.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("id");
+        sb.append('=');
+        sb.append(((this.id == null)?"<null>":this.id));
+        sb.append(',');
+        sb.append("name");
+        sb.append('=');
+        sb.append(((this.name == null)?"<null>":this.name));
+        sb.append(',');
+        sb.append("gender");
+        sb.append('=');
+        sb.append(((this.gender == null)?"<null>":this.gender));
+        sb.append(',');
+        sb.append("phone");
+        sb.append('=');
+        sb.append(((this.phone == null)?"<null>":this.phone));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
--- src/test/java/com/cydeo/step_definitions/SpartanFlowSteps.java
+++ src/test/java/com/cydeo/step_definitions/SpartanFlowSteps.java
@@ -1,6 +1,7 @@
 package com.cydeo.step_definitions;
 
 
+import com.cydeo.pages.pojo.Spartan;
 import com.cydeo.utilities.ConfigurationReader;
 import io.cucumber.java.en.When;
 import static io.restassured.RestAssured.*;
@@ -14,6 +15,7 @@ public class SpartanFlowSteps {
     String spartanUrl = ConfigurationReader.get("spartan.apiUrl");
     String mockUrl = ConfigurationReader.get("mock.apiUrl");
     Response mockSpartanJSON;
+    Response postResponse;
 
     @When("User sends a request to Mock API for a mock Spartan Data")
     public void userSendsARequestToMockAPIForAMockSpartanData() {
@@ -24,6 +26,22 @@ public class SpartanFlowSteps {
 
     @When("User uses Mock Data to create a Spartan")
     public void userUsesMockDataToCreateASpartan() {
+
+        Spartan mySpartan = new Spartan();
+        mySpartan.setName(mockSpartanJSON.path("name")); // reading Mock API spartan response and path is name
+        mySpartan.setGender(mockSpartanJSON.path("gender"));
+        Long phone = Long.valueOf(mockSpartanJSON.path("phone").toString());
+        mySpartan.setPhone(phone);
+        System.out.println(mySpartan);
+
+        postResponse = given()
+                .accept(ContentType.JSON) // I want JSON response
+                .contentType(ContentType.JSON) // I am sending JSON body
+                .and().body(mySpartan)  // serialize from JAVA to JSON
+
+                .when().post(spartanUrl+"/api/spartans");
+
+        postResponse.prettyPrint();
 
 
     }
