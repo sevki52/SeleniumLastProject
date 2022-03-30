eede60687f74b37de587f2926430f3417a3de73f
Sevki <sevki.oksuz@gmail.com>
2022-03-30 17:21:30 +0200

    waits are added

--- src/test/java/com/cydeo/eu7UIBootcamp/BasicNavgationWithTestNG.java
+++ src/test/java/com/cydeo/eu7UIBootcamp/BasicNavgationWithTestNG.java
@@ -1,22 +1,29 @@
 package com.cydeo.eu7UIBootcamp;
-
-
-
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class BasicNavgationWithTestNG {
 
         WebDriver driver;
+        WebDriverWait wait;
         @BeforeMethod
         public void setUp(){
             driver = WebDriverFactory.getDriver(ConfigurationReader.get("browser"));
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
             driver.get(ConfigurationReader.get("webTable.url"));
+            wait = new WebDriverWait(driver,10);  // explicit wait object
         }
         @AfterMethod
         public void tearDown(){
@@ -37,7 +44,11 @@ public class BasicNavgationWithTestNG {
             driver.findElement(By.name("password")).sendKeys(ConfigurationReader.get("web.table.pw"));
 
             // using text with xpath
-            driver.findElement(By.xpath("//button[.='Login']")).click();
+            WebElement buttonElement = driver.findElement(By.xpath("//button[.='Login']"));
+
+            // specific condition to a single webelement
+            wait.until(ExpectedConditions.elementToBeClickable(buttonElement));
+            buttonElement.click();
 
             String expectedUrl = "https://web-table-2.cydeo.com/orders";
             String actualUrl = driver.getCurrentUrl();
